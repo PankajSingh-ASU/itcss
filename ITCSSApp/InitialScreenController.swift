e2f3625e591f4e489f6617e80f979da18d9e4618
Pankaj Singh <PankajSingh.asu@gmail.com>
2016-06-12 12:20:42 -0700

    Corrected initial screen for iPad

--- ITCSSApp/InitialScreenController.swift
+++ ITCSSApp/InitialScreenController.swift
@@ -32,6 +32,7 @@ class InitialScreenController: UICollectionViewController {
                     "Tempe.jpg",
                     "West.jpg"
         ]
+        
     }
     override func numberOfSectionsInCollectionView(collectionView:
         (UICollectionView!)) -> Int {
@@ -43,14 +44,20 @@ class InitialScreenController: UICollectionViewController {
         return campuses.count
     }
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        
+       // NSLOG("Inside Image adding")
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier,
         forIndexPath: indexPath) as! InitialViewCell
         
         // Configure the cell
         let image = UIImage(named: campuseImages[indexPath.row])
         cell.imageView.image = image
-        cell.imageView.frame.size = CGSize(width: cellDimension!.height-4,height: cellDimension!.width)
+        
+
+//        cell.imageView.frame = cell.bounds
+//        cell.imageView.clipsToBounds = true
+        
+        //cell.contentView addSubview:imageView];
+        //cell.imageView.frame.size = CGSize(width: cellDimension!.height-4,height: cellDimension!.width)
         cell.name.text = campuses[indexPath.row]
         
         return cell
@@ -75,13 +82,29 @@ class InitialScreenController: UICollectionViewController {
     func collectionView(collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                                sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        
+        //NSLOG("Inside Size setting adding")
         let picDimension = self.view.frame.size.width / 2
         let picDimensionheight = self.view.frame.size.height / 3
     
         cellDimension = CGSizeMake(picDimension-5, picDimensionheight-2)
         return cellDimension!
         //return CGSizeMake(picDimension-1, picDimension-1)
+    }
+    override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
+        NSLog("Inside shouldPerformSegueWithIdentifier")
+        if identifier == "campusBuildings" {
+            NSLog("Inside identifier if")
+            if header?.reporter.text == nil || header?.reporter.text == ""
+            {
+                let alertController = UIAlertController(title: "Reporter Required", message:
+                    "Please Enter Reporter Name", preferredStyle: UIAlertControllerStyle.Alert)
+                alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+                
+                self.presentViewController(alertController, animated: true, completion: nil)
+                return false
+            }
+        }
+        return true
     }
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         NSLog("Inside prepareForSegue")
@@ -97,8 +120,7 @@ class InitialScreenController: UICollectionViewController {
             viewController.campus = selectedCampus
             viewController.reporter =  (header?.reporter.text!)!
         }
-
+        }
     }
-}
-    
+        
 }
